2fb307a809cdecf2b4c093e3f234a090a95f817c
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-10-10 16:22:53 +0900

    about 페이지 개발중

--- svelte-front-web/src/lib/components/about/AboutPage01.svelte
+++ svelte-front-web/src/lib/components/about/AboutPage01.svelte
@@ -1,3 +1,43 @@
 <script>
+  import BackImage from '$lib/images/study.jpg';
+  import BlackOut from '../BlackOut.svelte';
+
+  let preFunction;
+
+  function clickWindow() {
+
+  }
   
 </script>
+
+<div on:click={clickWindow}>
+  <div style="position:relative; z-index:10; color:white;">
+    <h1 style="margin-top: 50px;">3kkkk</h1>
+  </div>
+  <div style="position:realtive; z-index:1;">
+    <img class="imgBox" src={BackImage}/>
+  </div>
+  <BlackOut zIndex=2/>
+</div>
+
+
+
+<style>
+  .imgBox {
+    display:absolute;
+    top: 0;
+    left: 0;
+    width: auto;
+    height: 100vh;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    background-color: black;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    vertical-align: middle;
+  }
+  .progress-bar {
+transition: all 2.0s;
+}
+</style>
--- svelte-front-web/src/routes/about/+page.svelte
+++ svelte-front-web/src/routes/about/+page.svelte
@@ -1,7 +1,33 @@
 <script>
   import AboutMain from "$lib/components/about/AboutMain.svelte";
+  import AboutPage01 from "$lib/components/about/AboutPage01.svelte";
 
-
+  let n = 5;
+  let v = 50;
 </script>
 
-<AboutMain />
+<div style="width:100%; text-align:center; position:absolute; z-index:10;">
+  <div style="width: 80%">
+    <div style="width: 50px; height:50px; border-radius: 50%; background-color:gray;"/>
+  </div>
+  <div
+  class="progress"
+  role="progressbar"
+  aria-valuenow={v}
+  aria-valuemin="0"
+  aria-valuemax="100"
+  style="width: 80%; 
+  border: 2px solid white; 
+  margin-left:auto;
+  margin-right:auto;"
+>
+  <div
+    class="progress-bar progress-bar-striped progress-bar-animated"
+    style="width: {v}%;"
+  >
+    %
+  </div>
+</div>
+</div>
+
+<AboutPage01 />
