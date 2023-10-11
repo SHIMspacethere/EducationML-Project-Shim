768553ded6ba7b44ca8670736944efabc8950f3c
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-10-12 02:57:22 +0900

    h2 컴포넌트
    +애니메이션 효과

---
+++ svelte-front-web/src/lib/components/H2.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { fade, fly } from "svelte/transition";
+  let _style = "";
+  let _class = "";
+  export let sec = 500;
+  export { _style as style };
+  export { _class as class };
+</script>
+
+<h2
+  class={_class}
+  style={_style}
+  in:fly={{ delay: 0, y: 200, duration: sec }}
+  out:fade={{ duration: 500 }}
+>
+  <slot />
+</h2>
