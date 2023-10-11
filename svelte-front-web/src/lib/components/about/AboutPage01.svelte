aec03ff29254a7cbf38e5ad567b1092ea6ebcf27
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-10-12 02:57:33 +0900

    Update AboutPage01.svelte

--- svelte-front-web/src/lib/components/about/AboutPage01.svelte
+++ svelte-front-web/src/lib/components/about/AboutPage01.svelte
@@ -1,21 +1,69 @@
 <script>
+  import H2 from "../H2.svelte";
   import { fade, fly } from "svelte/transition";
-  import BackImage from "$lib/images/study.jpg";
-  import BlackOut from "../BlackOut.svelte";
+  import BackImage from "$lib/images/moe.jpg";
+  import ImgBoard from "../ImgBoard.svelte";
+
+  import AIHUB from "$lib/images/about/post1.png";
 
   export let onClose = () => {};
+  let count = 0;
+  const max = 5;
+  let intervalFunc;
+
+  function clickBoard() {
+    if (count >= max) onClose();
+    else {
+      count = max;
+    }
+  }
+
+  {
+    intervalFunc = setInterval(() => {
+      count++;
+      if (count == max) clearInterval(intervalFunc);
+    }, 1500);
+  }
 </script>
 
 <div
-  on:click={onClose}
+  on:click={clickBoard}
   in:fade={{ delay: 500, duration: 500 }}
   out:fade={{ duration: 500 }}
 >
   <div style="position:relative; z-index:3; color:white;">
-    <h1 style="margin-top: 100px;">3kkkk</h1>
+    <div style="margin-top: 120px; text-align:center; word-break:keep-all;">
+      {#if count >= 1}
+        <H2>전 세계는 'AI 혁명'에 빠져있습니다.</H2>
+      {/if}
+      {#if count >= 2}
+        <H2>이 흐름은 우리나라 교육 시장에도 파장이 미치고 있습니다.</H2>
+      {/if}
+      <div style="margin-top:50px;" />
+      {#if count >= 3}
+        <H2>교육부에서는 2025년부터 'AI 디지털교과서' 사업에,</H2>
+      {/if}
+      {#if count >= 4}
+        <H2
+          >과학기술정보통신부에서도 '수학문제 자동풀이 AI' 사업에 지원하고
+          있습니다.</H2
+        >
+      {/if}
+    </div>
+    <div style="margin-top:100px;" />
+    {#if count >= 5}
+      <div
+        class="d-block d-md-flex"
+        style="display:flex; gap:100px; justify-content:center;"
+      >
+        <ImgBoard src={AIHUB} />
+        <div style="margin-top:50px;" />
+        <ImgBoard />
+      </div>
+    {/if}
   </div>
   <div style="position:relative; color:black;">
-    <img class="imgBox" src={BackImage} style="z-index:1;"/>
+    <img class="imgBox" src={BackImage} style="z-index:1; object-fit: cover" />
   </div>
 </div>
 
@@ -24,7 +72,7 @@
     display: absolute;
     top: 0;
     left: 0;
-    width: auto;
+    width: 100%;
     height: 100vh;
     overflow-x: hidden;
     overflow-y: hidden;
@@ -33,6 +81,6 @@
     display: flex;
     justify-content: center;
     vertical-align: middle;
-    filter: brightness(30%)
+    filter: brightness(30%);
   }
 </style>
