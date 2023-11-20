d92354485322cc99dce7606eb0a24756fa976044
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-11-20 12:44:49 +0900

    모의고사 데이터 추가

--- svelte-front-web/src/lib/components/contents/collectorData.js
+++ svelte-front-web/src/lib/components/contents/collectorData.js
@@ -1,10 +1,30 @@
-import c2409 from './ce/c2409.js'
+import c2409 from "./ce/c2409.js";
+import c2406 from "./ce/c2406.js";
+import c2311 from "./ce/c2311.js";
+import c2309 from "./ce/c2309.js";
+import c2306 from "./ce/c2306.js";
 
-
-const data = [{
-  category: "2024학년도 9월 모의평가",
-  images: c2409
-},
+const data = [
+  {
+    category: "2024학년도 9월 모의평가",
+    images: c2409,
+  },
+  {
+    category: "2024학년도 6월 모의평가",
+    images: c2406,
+  },
+  {
+    category: "2023학년도 수능 (11월)",
+    images: c2311,
+  },
+  {
+    category: "2023학년도 9월 모의평가",
+    images: c2309,
+  },
+  {
+    category: "2023학년도 6월 모의평가",
+    images: c2306,
+  },
 ];
 
 export default data;
