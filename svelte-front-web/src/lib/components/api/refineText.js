5ed21209c9e726a4b9185bc465b03c2a4f1c123a
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-12-07 16:52:43 +0900

    최종보고서

--- svelte-front-web/src/lib/components/api/refineText.js
+++ svelte-front-web/src/lib/components/api/refineText.js
@@ -7,10 +7,12 @@ function refineText(el) {
   for (let i=0; i<mark.length; i++) {
     const cutOffIndex = text.indexOf(mark[i]);
     if (cutOffIndex !== -1) {
-      refinedText.push(text.substring(0, cutOffIndex + mark[i].length));
+      refinedText.push(text.substring(0, cutOffIndex + 
+        mark[i].length));
     }
   }
-  const minText = refinedText.reduce((min, cur) => (min.length <= cur.length ? min : cur));
+  const minText = refinedText.reduce((min, cur) => 
+    (min.length <= cur.length ? min : cur));
   return minText;
 }
 
